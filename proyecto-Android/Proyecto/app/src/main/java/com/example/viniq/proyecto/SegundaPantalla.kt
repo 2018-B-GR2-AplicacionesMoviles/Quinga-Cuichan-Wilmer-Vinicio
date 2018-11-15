23e7b5ed7c40b923edf14653d6b158dae1082fa0
davini10c <vinicio_520@hotmail.com>
2018-11-15 00:39:11 -0500

    proyectto

---
+++ proyecto-Android/Proyecto/app/src/main/java/com/example/viniq/proyecto/SegundaPantalla.kt
@@ -0,0 +1,12 @@
+package com.example.viniq.proyecto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaPantalla : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_pantalla)
+    }
+}
