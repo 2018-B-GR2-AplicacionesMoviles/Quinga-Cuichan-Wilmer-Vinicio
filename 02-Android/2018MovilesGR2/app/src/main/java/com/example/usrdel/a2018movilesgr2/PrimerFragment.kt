9d4ae46e9abca4a3b7a09f07994a7e1f4b85be59
FerchTR <eddyferch@hotmail.es>
2019-01-04 14:22:30 -0500

    actualizar fragmentos

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/PrimerFragment.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/PrimerFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_primer.*
+import kotlinx.android.synthetic.main.lista_recycler_view_layout.*
 
 
 class PrimerFragment : Fragment() {
@@ -15,6 +16,8 @@ class PrimerFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+
         return inflater!!.inflate(
                 R.layout.fragment_primer, // XML A USARSE
                 container,
@@ -24,8 +27,11 @@ class PrimerFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-
+        val textoNombre = edit_text_nombre.text
+        val textoDescripcion = edit_text_descripcion.text
+        Log.i("bdd", "Vamos a recuperar los datos")
+        Log.i("bdd", "$textoNombre")
+        Log.i("bdd", "$textoDescripcion ")
 
         if (arguments != null) {
 
@@ -35,8 +41,16 @@ class PrimerFragment : Fragment() {
                     .text = arguments!!.getString("nombre")
 
         }
+    }
 
+    override fun onStop() {
+        super.onStop()
 
+        val textoNombre = edit_text_nombre.text
+        val textoDescripcion = edit_text_descripcion.text
+        Log.i("bdd", "Vamos a guardar los datos")
+        Log.i("bdd", "$textoNombre")
+        Log.i("bdd", "$textoDescripcion ")
     }
 
 }
